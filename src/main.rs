b953c68e4dffe204dd2ba16fc294acf09cd45eb2
Krishna Addepalli <coolkrishna31@gmail.com>
2024-05-17 22:11:09 +0200

    Expand the initial page

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,20 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <div>
-            <h1>{"Hello, World!"}</h1>
-        </div>
+        <>
+            <h1>{ "RustConf Explorer" }</h1>
+            <div>
+                <h3>{"Videos to watch"}</h3>
+                <p>{ "Krishna Addepalli: Building and breaking things with Rust" }</p>
+                <p>{"Jane Smith: The development process "}</p>
+                <p>{"Matt Miller: The Web 7.0"}</p>
+                <p>{"Tom Jerry: Mouseless development"}</p>
+            </div>
+            <div>
+                <h3>{"John Doe: Building and breaking things"}</h3>
+                <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+            </div>
+        </>
     }
 }
 fn main() {
