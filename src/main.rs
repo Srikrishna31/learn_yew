e473125b918f19b012b5a2c24e70ec0c59ff74da
Krishna Addepalli <coolkrishna31@gmail.com>
2024-05-17 20:23:05 +0200

    Add initial html page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <div>
+            <h1>{"Hello, World!"}</h1>
+        </div>
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
